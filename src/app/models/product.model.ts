391404b63b85d2b17a93073e9fe94c3a538eea5d
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-04-21 16:00:34 +0530

    added product.model.ts

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  description: string;
+  stockCount: number;
+  addedDate: Date;
+  rating: number;
+}
